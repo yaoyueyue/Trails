47ec4d3a2b8613a78045d5e29f4d1a3b80a5afd3
Yue Yao <amanday0021@gmail.com>
2023-07-26 18:56:47 -0400

    fixed mapbox and cloudinary configuration and js object conversion problem

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -61,7 +61,7 @@ module.exports.showCampground = async (req, res,) => {
         }
     }).populate('author');
     if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
+        req.flash('error', 'Cannot find that Trail!');
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/show', { campground });
@@ -71,7 +71,7 @@ module.exports.renderEditForm = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id)
     if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
+        req.flash('error', 'Cannot find that trail!');
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/edit', { campground });
@@ -90,13 +90,13 @@ module.exports.updateCampground = async (req, res) => {
         }
         await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
     }
-    req.flash('success', 'Successfully updated campground!');
+    req.flash('success', 'Successfully updated trail!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted campground')
+    req.flash('success', 'Successfully deleted trail')
     res.redirect('/campgrounds');
 }
--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -24,6 +24,9 @@ map.on('load', function () {
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
     });
+    // console.log(JSON.stringify(campgrounds, null, 2));
+
+
 
     map.addLayer({
         id: 'clusters',
